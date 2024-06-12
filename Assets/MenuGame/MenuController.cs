d8915528ab8f85b2f6789d16641d3ed0bb37d514
LegAcyMDN <121859179+LegAcyMDN@users.noreply.github.com>
2024-06-12 11:35:03 +0200

    ajout menu

--- Assets/MenuGame/MenuController.cs
+++ Assets/MenuGame/MenuController.cs
@@ -1,22 +1,35 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.SceneManagement;
+
+public enum PanelType { None, Main, Credits, }
 
 public class MenuController : MonoBehaviour
 {
+    [SerializeField] private List<MenuPanel> panelsList = new List<MenuPanel>();
+    private Dictionary<PanelType, MenuPanel> panelsDict = new Dictionary<PanelType, MenuPanel>();
+    private GameManager manager;
+    private void Start()
+    {
+        manager = GameManager.instance;
+
+        foreach (var panel in panelsList)
+        {
+            if (panel) { panelsDict.Add(panel.GetPanelType(), panel); }
+        }
+
+        OpenOnePanel(PanelType.Main);
+    }
+    private void OpenOnePanel(PanelType otherType)
+    {
+        foreach (var panel in panelsList) { panel.ChangeState(false); }
+
+        if (otherType != PanelType.None) { panelsDict[otherType].ChangeState(true); }
+    }
+    public void OpenPanel(PanelType otherType)
+    { OpenOnePanel(otherType); }
     public void JouerScene(int sceneJouer)
-    {
-        SceneManager.LoadScene(sceneJouer);
-    }
-
-    public void CreditScene(int creditScene)
-    {
-        SceneManager.LoadScene(creditScene);
-    }
-
+    { manager.JouerScene(sceneJouer); }
     public void Quitter()
-    {
-        Application.Quit();
-    }
+    { manager.Quitter(); }
 }
