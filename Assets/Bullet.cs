61a2a7d016a86a6a5c04670f5a277a3a64f412ab
devnatiofrance <106927551+devnatiofrance@users.noreply.github.com>
2024-06-13 12:45:47 +0200

    Ak47

---
+++ Assets/Bullet.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    void OnCollisionEnter(Collision collision)
+    {
+        // Vérifie si le joueur entre en collision avec un autre joueur
+        if (collision.gameObject.tag=="Tree")
+
+        Destroy(collision.gameObject);
+    }
+}
